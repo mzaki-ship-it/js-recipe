6a26a308df1874495aff13b9d6b3d5a1955e01ec
mzaki-ship-it <airmurray7575@gmail.com>
2021-05-15 17:58:51 +0900

    cat

--- 2_example/2/犬/main.js
+++ 2_example/2/犬/main.js
@@ -1,7 +1,7 @@
 const imageElement = document.getElementById("dog-image")
 
 // 指定したサーバーにデータを取りに行く
-fetch("https://dog.ceo/api/breeds/image/random")
+fetch("https://dog.ceo/api/breeds/image/random")g
   .then((res) => {
     return res.json() // 結果を json として読み込む
   })
